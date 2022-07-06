daa7e59bb73ca161e1ffd040c6f0920692014a95
Michel <michelssalves@gmail.com>
2022-07-06 13:04:09 -0300

    2022-07-06 11:58

--- registroDeAbastecimento.php
+++ registroDeAbastecimento.php
@@ -34,7 +34,7 @@ include 'assets/controllers/checkAcess.php';
                     <a class="w3-bar-item w3-button"><?= $usuario; ?></a>
                 </div>
                 <div class="w3-bar w3-light-grey w3-container">
-                <table class="table table-striped table-bordered">
+                <table class="table table-bordered">
                 <thead class="thead-dark">
                     <tr>
                         <th><center>Acertos</th>
